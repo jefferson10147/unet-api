4ad419cfc043ee95524029ae68f931df4a46e372
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-03 19:41:22 -0400

    Starting factory design pattern from flask

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+
+def created_app():
+    app = Flask(__name__)
+    return app
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from app import created_app
+
+app = created_app()
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World hey!'
