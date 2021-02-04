1aee3cad70017f398c463466ef972645dd6fe9d1
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-03 21:21:47 -0400

    Setting env variables, using decouple librarie

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 def created_app():
     app = Flask(__name__)
     return app
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import created_app
 
-app = created_app()
 
+app = created_app()
 
 @app.route('/')
 def hello_world():
