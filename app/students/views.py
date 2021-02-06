288f0d945fe9c6413c414a1ab2a6c945ec9e1748
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-06 16:10:51 -0400

    Changing api url's and adding a new form to search by dni

--- app/students/views.py
+++ app/students/views.py
@@ -7,42 +7,42 @@ from .models import StudentsModel
 students_blueprint = Blueprint(
     name="students",
     import_name=__name__,
-    url_prefix="/api/students"
+    url_prefix="/api/v1"
 )
 students_model = StudentsModel()
 
 
-@students_blueprint.route('/')
+@students_blueprint.route("/search/students")
 def all_students():
     db_response = students_model.get_all_students()
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/id/<string:id>")
+@students_blueprint.route("/search/id/<string:id>")
 def student_by_id(id):
     db_response = students_model.get_student_by_id(ObjectId(id))
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/name/<string:name>")
+@students_blueprint.route("/search/name/<string:name>")
 def students_by_name(name):
     db_response = students_model.get_students_by_name(name)
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/second_name/<string:second_name>")
+@students_blueprint.route("/search/second_name/<string:second_name>")
 def students_by_second_name(second_name):
     db_response = students_model.get_students_by_second_name(second_name)
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/lastname/<string:lastname>")
+@students_blueprint.route("/search/lastname/<string:lastname>")
 def students_by_lastname(lastname):
     db_response = students_model.get_students_by_lastname(lastname)
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/second_lastname/<string:second_lastname>")
+@students_blueprint.route("/search/second_lastname/<string:second_lastname>")
 def students_by_second_lastname(second_lastname):
     db_response = students_model.get_students_by_second_lastname(
         second_lastname
@@ -50,7 +50,7 @@ def students_by_second_lastname(second_lastname):
     return Response(json_util.dumps(db_response), mimetype="application/json")
 
 
-@students_blueprint.route("/dni/<string:dni>")
+@students_blueprint.route("/search/dni/<string:dni>")
 def student_by_dni(dni):
-    db_response = students_model.get_student_by_dni(dni)
+    db_response = students_model.get_student_by_dni(''.join(['V',dni]))
     return Response(json_util.dumps(db_response), mimetype="application/json")
