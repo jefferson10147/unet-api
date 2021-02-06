55f228963dbb1d77e3792769518222d54cd32cc4
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-06 16:00:45 -0400

    Implementing all endpoints of simple search in each database field

--- app/students/models.py
+++ app/students/models.py
@@ -4,9 +4,22 @@ from app.extensions import mongo
 class StudentsModel():
 
     def get_all_students(self):
-        students = mongo.db.students.find({}, {"_id": 0})
-        return students
+        return mongo.db.students.find({}, {"_id": 0})
 
     def get_student_by_id(self, id):
-        student = mongo.db.students.find_one({"_id": id}, {"_id": 0})
-        return student
+        return mongo.db.students.find_one({"_id": id}, {"_id": 0})
+
+    def get_student_by_dni(self, dni):
+        return mongo.db.students.find({"dni": dni.capitalize()}, {"_id": 0})
+
+    def get_students_by_name(self, name):
+        return mongo.db.students.find({"name": name.capitalize()})
+
+    def get_students_by_second_name(self, second_name):
+        return mongo.db.students.find({"second_name": second_name.capitalize()}, {"_id": 0})
+
+    def get_students_by_lastname(self, lastname):
+        return mongo.db.students.find({"lastname": lastname.capitalize()}, {"_id": 0})
+
+    def get_students_by_second_lastname(self, second_lastname):
+        return mongo.db.students.find({"second_lastname": second_lastname.capitalize()}, {"_id": 0})
